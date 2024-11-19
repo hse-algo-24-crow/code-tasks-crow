085d866a098d4a83267a261f81610421b5fde144
Murat <S.diykstra@mail.ru>
2024-11-19 17:41:01 +0300

    test-commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from profilehooks import profile
 
 
+@profile
 def fibonacci_rec(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
     формуле вычисления последовательности.
@@ -8,9 +9,15 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
+@profile
 def fibonacci_iter(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
     массива для хранения вычисляемых данных.
@@ -18,18 +25,34 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1    
+    fib = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
 
 
+@profile
 def fibonacci(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
-
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    prev, curr = 0, 1
+    for _ in range(2, n + 1):
+        prev, curr = curr, prev + curr
+    return curr
 
 
+@profile
 def main():
     n = 35
     print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
@@ -39,8 +62,9 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
-if __name__ == "__main__":
+if __name__ == "main":
     main()
+
