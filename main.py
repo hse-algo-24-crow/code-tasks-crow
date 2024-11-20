dd3b59b146258439f2439270f550a5e101943820
Kiborg010 <evgeni.kibardin@yandex.ru>
2024-11-20 20:42:58 +0500

    first

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 0:
+        raise ValueError("Номер числа должен быть положительным")
+    else:
+        if n == 1 or n == 2:
+            return 1
+        else:
+            return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
 def fibonacci_iter(n: int) -> int:
@@ -18,7 +24,16 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 0:
+        raise ValueError("Номер числа должен быть положительным")
+    else:
+        if n == 1 or n == 2:
+            return 1
+        else:
+            array = [1, 1]
+            for i in range(n - 2):
+                array.append(array[-2] + array[-1])
+            return array[-1]
 
 
 def fibonacci(n: int) -> int:
@@ -27,7 +42,19 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 0:
+        raise ValueError("Номер числа должен быть положительным")
+    else:
+        if n == 1 or n == 2:
+            return 1
+        else:
+            first_number = 1
+            second_number = 1
+            for i in range(n - 2):
+                s = first_number + second_number
+                first_number = second_number
+                second_number = s
+            return second_number
 
 
 def main():
@@ -39,7 +66,7 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
 if __name__ == "__main__":
