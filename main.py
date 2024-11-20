8926ee0b32f623d211559ee1000cec0357316cdd
andres1m <andreusar11@gmail.com>
2024-11-20 18:34:16 +0500

    Выполнено задание 1

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+
+    if n <= 2:
+        return 1
+
+    return fibonacci_rec(n-1) + fibonacci_rec(n-2)
     pass
 
 
@@ -18,6 +23,15 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    arr = [1, 1]
+
+    if n <= 2:
+        return arr[n-1]
+
+    for i in range(2, n):
+        arr.append(arr[i-1] + arr[i-2])
+
+    return arr[n-1]
     pass
 
 
@@ -27,6 +41,18 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if n <= 2:
+        return 1
+
+    f, s = 1, 1
+    curr = 1
+
+    for i in range(2, n):
+        curr = f + s
+        f = s
+        s = curr
+    return curr
+
     pass
 
 
@@ -39,7 +65,7 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
 if __name__ == "__main__":
