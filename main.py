436c1312061908c27084980a73bbc6199142bcff
Zaozerov Bogdan <zaobogdan@yandex.ru>
2024-11-26 14:56:50 +0500

    Решение 1 варианта задания 2

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+def checks(matrix: list[list[int]]) -> bool:
+    if not isinstance(matrix, list):
+        raise Exception("Переданный объект не является матрицей. Матрица должна быть представлена в виде списка списков")
+    
+    if len(matrix) == 0:
+        raise Exception("Передана пустая матрица. Посчитать определитель невозможно")
+    
+    for row in matrix:
+        if not isinstance(row, list):
+            raise Exception("Переданный объект не является матрицей. Матрица должна быть представлена в виде списка списков")
+        
+        if len(row) != len(matrix):
+            raise Exception("Переданный объект не является квадратной матрицей. Количество строк должно совпадать с количеством столбцов")
+        
+        for elem in row:
+            if not isinstance(elem, int):
+                raise Exception("В матрице есть нецелочисленные типы данных. Матрица должна состоять из целых чисел")
+    
+    return True
+
+
 def calculate_determinant(matrix: list[list[int]]) -> int:
     """Вычисляет определитель целочисленной квадратной матрицы
 
@@ -6,7 +27,25 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    checks_passed = False
+    if not checks_passed:
+        checks(matrix)
+        checks_passed = True
+
+
+    det = 0
+    x = 1  # -1**(i+j)
+    size = len(matrix)
+    if size == 1:
+        return matrix[0][0]
+    
+    else:
+        for j in range(size):
+            new_matrix = [items[:j] + items[j + 1:] for items in matrix[1:]]
+            det += x * matrix[0][j] * calculate_determinant(new_matrix)
+            x = -1 * x
+
+    return det
 
 
 def main():
